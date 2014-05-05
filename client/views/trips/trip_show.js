a16665890fc578fb84df60088ba3eca3e0849bd7
markgaucher <markgaucher@gmail.com>
2014-05-04 22:43:16 -0400

    improved wireframe for trip page

---
+++ client/views/trips/trip_show.js
@@ -0,0 +1,19 @@
+
+Template.tripShow.rendered = function() {
+
+  GoogleMaps.init({
+    'sensor': true, //optional
+    //'key': 'MY-GOOGLEMAPS-API-KEY', //optional
+    'language': 'en' //optional
+  },
+    function() {
+      var mapOptions = {
+        zoom: 10,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+      map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+      map.setCenter(new google.maps.LatLng( 43.65323, -79.38318 ));
+    }
+  );
+
+};
